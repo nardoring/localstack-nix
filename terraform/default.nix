d1531ed69dbfd4e15e5708169fdbc8387b7492ec
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2024-03-24 05:40:05 -0600

    bump tflocal version

--- terraform/default.nix
+++ terraform/default.nix
@@ -4,19 +4,22 @@
   fetchPypi,
   bc-python-hcl2,
   localstack-client,
+  packaging,
 }:
 buildPythonApplication rec {
   pname = "terraform-local";
-  version = "0.16.0";
+  version = "0.18.1";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-MZMhPFQnXVokB9AXY3eKh25FoE2qpIxKaYpkgH11eh4=";
+    # hash = "sha256-MZMhPFQnXVokB9AXY3eKh25FoE2qpIxKaYpkgH11eh4="; # 0.16.0
+    hash = "sha256-dx/nPts3obTbU61kHPQ1Tqb+eseEnra0emnzPJkJ3zU="; # 0.18.1
   };
 
   doCheck = true;
 
   nativeBuildInputs = [
+    packaging
     bc-python-hcl2
     localstack-client
   ];
